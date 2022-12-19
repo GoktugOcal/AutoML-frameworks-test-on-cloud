355e15c3c8e33ab6ca6c902dbeddf8e98a91bfc2
goktugocal <goktugocal41@gmail.com>
2022-12-20 00:22:53 +0300

    mini

--- H2O/test/classificationTest_1.py
+++ H2O/test/classificationTest_1.py
@@ -3,9 +3,12 @@ from datetime import datetime
 import h2o
 from h2o.automl import H2OAutoML
 
-h2o.connect(
-    url = "http://34.79.223.138:80"
-)
+# h2o.connect(
+    # url = "http://34.79.223.138:80"
+# )
+h2o.init()
+
+
 # Use local data file or download from GitHub
 import os
 docker_data_path = "/home/h2o/data/automl/product_backorders.csv"
@@ -25,12 +28,14 @@ x.remove(y)
 x.remove("sku")
 
 s = time()
-aml = H2OAutoML(max_models = 10, seed = 1, project_name = "classification_test_" + datetime.now().strftime("%Y-%m-%dT%H.%M.%S"))
+aml = H2OAutoML(
+    max_models = 10,
+    seed = 1,
+    include_algos = ["DRF"],
+    project_name = "classification_test_" + datetime.now().strftime("%Y-%m-%dT%H.%M.%S"))
 aml.train(x = x, y = y, training_frame = df)
 
 print("Execution Time :", time() - s)
 
 lb = aml.leaderboard
 print(lb)
-
-# h2o.remove(aml)
--- H2O/test/classificationTest_2.py
+++ H2O/test/classificationTest_2.py
@@ -1,11 +1,14 @@
+import pandas as pd
 from time import time
 from datetime import datetime
 import h2o
 from h2o.automl import H2OAutoML
+from scipy.io.arff import loadarff
+# h2o.connect(
+#     url = "http://34.79.223.138:80"
+# )
 
-h2o.connect(
-    url = "http://34.79.223.138:80"
-)
+h2o.init()
 
 raw_data = loadarff('data/ElectricDevices_TRAIN.arff')
 df_data = pd.DataFrame(raw_data[0])
@@ -15,11 +18,25 @@ hf = h2o.H2OFrame(df_data)
 y = "target"
 x = hf.columns
 
+
+# ALGORITHMS
+#
+# DRF 
+# GLM
+# XGBoost
+# GBM
+# DeepLearning
+# StackedEnsemble
+
 s = time()
-aml = H2OAutoML(max_models = 10, seed = 1,  project_name = "classification_test_" + datetime.now().strftime("%Y-%m-%dT%H.%M.%S"))
+aml = H2OAutoML(
+    max_models = 10,
+    seed = 1,
+    include_algos = ["DRF"],
+    project_name = "classification_test_" + datetime.now().strftime("%Y-%m-%dT%H.%M.%S"))
 aml.train(x = x, y = y, training_frame = hf)
 print("Execution Time :", time() - s)
 
 lb = aml.leaderboard
 
-h2o.remove(aml)
+print(lb)
